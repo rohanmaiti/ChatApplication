69f4ef9df62bbea813f7e75409f35b3df2693a06
rohanmaiti <maitirohan2019@gmail.com>
2025-01-29 10:36:18 +0530

    Login pages validateform and hable submit updated

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -3,6 +3,7 @@ import { useAuthStore } from "../store/useAuthStore";
 import { Link } from "react-router-dom";
 import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare } from "lucide-react";
 import { useState } from "react";
+import toast from 'react-hot-toast';
 
 export const LoginPage = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -20,7 +21,7 @@ export const LoginPage = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const success = validateForm();
-    if(success)
+    if(success === true)
     login(formData);
   };
   
