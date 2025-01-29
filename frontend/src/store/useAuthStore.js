26ba48d4d7c3f65b33a16cf2e2ca0f9a2e7415d0
rohanmaiti <maitirohan2019@gmail.com>
2025-01-29 10:37:26 +0530

    Login logic updated sucessfully

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -22,25 +22,38 @@ export const useAuthStore = create((set)=>{
             }
         },
         signup: async(data)=>{
+
          try {
             set({ isSigningUp: true });
             const res = await axiosInstance.post("/auth/signup", data);
             set({ authUser: res.data });
             toast.success("Account created successfully");
-            get().connectSocket();
           } catch (error) {
             toast.error(error.response.data.message);
           } finally {
             set({ isSigningUp: false });
           }
         },
-        
+
+        login: async (data)=>{
+            try {
+              set({isLoggingIng : true})
+              const res = await axiosInstance.post("/auth/login",data);
+              set({ authUser: res.data });
+              toast.success("Logged in successfully");
+            } catch (error) {
+              toast.error(error.response.data.message);
+            }
+            finally{
+              set({isLoggingIng : false})
+            }
+        },
+      
         logout: async () => {
             try {
               await axiosInstance.post("/auth/logout");
               set({ authUser: null });
               toast.success("Logged out successfully");
-              get().disconnectSocket();
             } catch (error) {
               toast.error(error.response.data.message);
             }
