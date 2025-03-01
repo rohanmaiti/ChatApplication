dc3dcfe5b7f77e6a080f770a00844d43c9989e6a
Rohan Maiti <maitirohan2019@gmail.com>
2025-03-01 23:41:28 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import { useAuthStore } from "../store/useAuthStore";
 import { LogOut, MessageSquare, Settings, User } from "lucide-react";
-
 export const Navbar = () => {
   const { logout, authUser } = useAuthStore();
 
